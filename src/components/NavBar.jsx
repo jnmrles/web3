867d630c4a0fdf9ae9ee18f42e71bbc583a4496c
JM <jnmrles@gmail.com>
2022-01-18 14:35:32 -0600

    Welcome section, eth card, and form

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () =>{
 
       ))}
       <li className="bg-[#2592e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-        Taylor
+        Login
 
       </li>
 
@@ -40,11 +40,11 @@ const NavBar = () =>{
       : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={()=>{setToggleMenu(true)}}/>
     }
     {toggleMenu && (
-      <ul>
+      <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
         <li className="text-xl w-full my-2">
         <AiOutlineClose onClick={()=>setToggleMenu(false)} />
         {["Market","Exchange","Tutorials","Wallets"].map((item,index)=>(
-        <NavBarItem key={item +index} title={item}/>
+        <NavBarItem key={item +index} title={item} classProps="my-2 text-lg"/>
 
       ))}
 
