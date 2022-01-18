a76a86b769d3212ffd5eda4b6f0391235146f61d
JM <jnmrles@gmail.com>
2022-01-17 18:19:42 -0600

    file structure and dependencies

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import {HiMenuAlt4} from 'react-icons/hi'
+import {AiOutlineClose} from 'react-icons/ai'
+
+import logo from '../../images/logo.png'
+
+const NavBar = () =>{
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">Navbar</nav>
+  )
+}
+
+export default NavBar
